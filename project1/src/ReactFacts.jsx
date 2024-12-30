1cbbec16b8391e03ad4575496b75d3571b97e48c
Johan Almanzar <almanzar.j@northeastern.edu>
2024-12-29 21:30:44 -0500

    Created a Page component to beat a challenge, and rendered it!

--- project1/src/ReactFacts.jsx
+++ project1/src/ReactFacts.jsx
@@ -17,7 +17,7 @@ const root = createRoot(document.getElementById("root"))
 root.render(
     <main>
         <img src={reactLogo} className="logo react" alt="React logo" width="40px" />
-        <h1>Fun facts about React</h1>
+        <h1>Fun facts about React!</h1>
         <ul>
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
@@ -25,5 +25,27 @@ root.render(
             <li>Is maintained by Meta</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
+        <h1>I am excited to learn React because:</h1>
+        <Page />
     </main>
 )
+
+// new challenge
+
+/**
+ * Create a custom "Page" component
+ * 
+ * It should return an ordered list with the reasons why you're
+ * excited to be learning react :)
+ * 
+ * Render the Page component.
+ */
+
+function Page() {
+    return (
+        <ol>
+            <li>Getting better as a SWD!</li>
+            <li>Adding more tools to my kit!</li>
+        </ol>
+    )
+}
