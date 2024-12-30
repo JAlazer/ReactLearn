ade352f9a1ae558b7a63a5f7d82f88ec78efbcd0
Johan Almanzar <almanzar.j@northeastern.edu>
2024-12-30 01:32:48 -0500

    Clean slate, for the actual project!

--- project1/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <small>
-                © 2024 Almanzar development. All rights reserved.
-            </small>
-        </footer>
-    )
-}
--- project1/src/ReactFacts.jsx
+++ project1/src/ReactFacts.jsx
@@ -1,58 +1,19 @@
-
 import { createRoot } from "react-dom/client"
-import reactLogo from './assets/react.svg'
-
-// now to add styling!
-import "./ReactFactStyle.css"
-
-// adding components!
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
-
+import { reactLogo } from "./assets/react"
 const root = createRoot(document.getElementById("root"))
 
-root.render(
-    <main>
-        <img src={reactLogo} className="logo react" alt="React logo" width="40px" />
-        <h1>Fun facts about React!</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k starts on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-        
-        <Page />
-    </main>
-)
-
-
-
-function Page() {
-    return (
-        // same as a Fragment which is the same as a div
-        <>
-            <Header />
-            <MainContent />
-            <Footer />
-        </>
-    )
-}
-
 /**
- * Challenge: Separate what needs to be created for ReactFacts
- * Two components within an App component
- * Header:
- *      img
- *      text - could be a span to keep img and text inline
+ * Project setup
  * 
- * MainContent:
- *      h1
- *      ul - with 5 li's
- *      img 
+ * create an App component in a separate file
+ * create a components folder
+ * create the following components (where h1 is the only thing)
+ *      - Navbar
+ *      - Main
+ * Have App component import and render the Navbar and Main components
+ * Import and render the App component in here using ReactDOM:
+ *       - Should have "Navbar goes here" etc. showing up in mini-browser
+ * Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+ *      Put the "<link />" to those fonts ABOVE the style.css link (Research how to get this done!)
  * 
  */
-
-
