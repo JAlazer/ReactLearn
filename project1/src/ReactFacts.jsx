4c0c7ad86b5f3a29d4ac25680d44f880f3f7f423
Johan Almanzar <almanzar.j@northeastern.edu>
2024-12-29 20:59:58 -0500

    Starting of ReactFacts without styling!

---
+++ project1/src/ReactFacts.jsx
@@ -0,0 +1,29 @@
+/**
+ * Challenge: Starting from scratch, build and render the 
+ * HTML for our section project. Check the Google slide for
+ * what you're trying to build.
+ * 
+ * adding styling later
+ * 
+ * Hints:
+ * set width attribute of the image element just like in HTML. In the slide, it is set to 40px
+ */
+
+import { createRoot } from "react-dom/client"
+import reactLogo from './assets/react.svg'
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <main>
+        <img src={reactLogo} className="logo react" alt="React logo" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k starts on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
