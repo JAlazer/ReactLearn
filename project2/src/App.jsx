84de60d55118c2c4f34b3be23d5aab4691f8cfa8
Johan Almanzar <almanzar.j@northeastern.edu>
2025-01-02 23:13:19 -0500

    React using javascript variables!

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -2,10 +2,14 @@ import Header from "./components/Header.jsx"
 import Entry from "./components/Entry.jsx"
 
 export default function App() {
+    const firstName = "Joe"
+    const lastName = "Schmoe"
+
     return (
         <>
             <Header />
             <Entry />
+            <h1>Hello {firstName} {lastName}</h1>
         </>
         )
 }
